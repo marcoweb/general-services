61eaabf14a89c2a30e0bda5533d93f6418107bbe
Marco Carvalho <marcocarvalho.web@gmail.com>
2021-06-21 19:32:42 -0300

    Login

--- App.js
+++ App.js
@@ -5,32 +5,14 @@ import { createStackNavigator } from '@react-navigation/stack';
 import Login from './pages/login'
 import Inicial from './pages/inicial'
 import UserForm from './pages/user-form'
-import firebase from './firebase';
 
 export default function App() {
-  const db = firebase.firestore()
   const Stack = createStackNavigator()
-
-  const [user, setUser] = useState(null)
-  const [userData, setUserData] = useState(null)
-
-  async function loginGoogle() {
-    const provider = new firebase.auth.GoogleAuthProvider()
-    const result = await firebase.auth().signInWithPopup(provider)
-    if(result){
-      setUser(result.user)
-      db.collection('usuarios').where('uid', '==', result.user.uid).get().then((querySnapshot) => {
-        if(querySnapshot.size == 1)
-          setUserData(querySnapshot[0].data())
-        return userData
-      })
-    }
-  }
 
   return (
       <NavigationContainer>
         <Stack.Navigator initialRouteName="Login">
-          <Stack.Screen name="Login" component={Login} initialParams={{googleLoginHandler : loginGoogle}} />
+          <Stack.Screen name="Login" component={Login} />
           <Stack.Screen name="Inicial" component={Inicial}/>
           <Stack.Screen name="UserForm" component={UserForm} />
         </Stack.Navigator>
